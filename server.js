35414f2a48b36f932ebfe10370eb40f2741016c4
Lpter <lephucthiep2785@gmail.com>
2023-05-18 17:24:03 +0700

    arjs

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+
+const port = 8080; // Cổng mà máy chủ sẽ lắng nghe
+
+// Áp dụng middleware CORS cho tất cả các route
+app.use(cors());
+
+// Đường dẫn đến thư mục chứa tệp HTML
+const publicDirectoryPath = path.join(__dirname, 'public');
+
+// Định tuyến để phục vụ tệp HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicDirectoryPath, 'index.html'));
+});
+
+// Khởi động máy chủ
+app.listen(port, () => {
+  console.log(`Server đang chạy trên cổng ${port}`);
+});
